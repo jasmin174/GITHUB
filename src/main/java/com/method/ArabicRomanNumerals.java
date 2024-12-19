fb4d7605456a146ad523809253e62918108913d5
jasmin174 <yasminesisbane@gmail.com>
2024-12-19 19:26:57 +0100

    test passed

--- src/main/java/com/method/ArabicRomanNumerals.java
+++ src/main/java/com/method/ArabicRomanNumerals.java
@@ -3,7 +3,9 @@ package com.method;
 public class ArabicRomanNumerals {
  public String convert (int nbr){
   if(nbr>2)
-  {return "II";}
+  {return "III";}
+  else if (nbr>1)
+  { return "II";}
   return "I";
  }
 }
