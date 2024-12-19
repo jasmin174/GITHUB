aab60e163308081c131ca9d247c579f38ce5cd8d
jasmin174 <yasminesisbane@gmail.com>
2024-12-19 19:40:01 +0100

    TEST FAILED we expected IV BUT WE HAVE III

--- src/test/java/com/testing/convertingTest.java
+++ src/test/java/com/testing/convertingTest.java
@@ -42,4 +42,18 @@ public class convertingTest {
         // THEN
         assertEquals(expected,result);
     }
+
+    @Test
+    public void convert_4_into_IV() {
+        // GIVEN
+        int input = 4;
+      String expected="IV";
+        ArabicRomanNumerals converter = new ArabicRomanNumerals();
+
+        // WHEN
+        String result = converter .convert(input);
+
+        // THEN
+        assertEquals(expected,result);
+    }
 }
