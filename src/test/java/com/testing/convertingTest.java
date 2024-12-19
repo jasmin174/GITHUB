033f5e636c6fd003bf59a1cca73fe476c6d7aa7a
jasmin174 <yasminesisbane@gmail.com>
2024-12-19 19:24:35 +0100

    TEST FAILED WE EXPECT III BUT WE HAD II

--- src/test/java/com/testing/convertingTest.java
+++ src/test/java/com/testing/convertingTest.java
@@ -29,4 +29,17 @@ public class convertingTest {
         // THEN
         assertEquals(expected,result);
     }
+    @Test
+    public void convert_3_into_II() {
+        // GIVEN
+        int input = 3;
+        String expected = "III";
+        ArabicRomanNumerals converter = new ArabicRomanNumerals();
+
+        // WHEN
+        String result = converter .convert(input);
+
+        // THEN
+        assertEquals(expected,result);
+    }
 }
